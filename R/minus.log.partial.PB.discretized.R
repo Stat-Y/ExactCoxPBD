621997b7374095b6e2eb7932b501d431277092f9
Stat-Y <70354673+Stat-Y@users.noreply.github.com>
2024-11-14 00:49:49 -0500

    Update minus.log.partial.PB.discretized.R

--- R/minus.log.partial.PB.discretized.R
+++ R/minus.log.partial.PB.discretized.R
@@ -15,6 +15,9 @@
 #' @return The returned value is minus log partial likelihood at a given slope.
 #'
 #' @examples
+#' library(poibin)
+#' library(survival)
+#' library(numDeriv)
 #' library(KMsurv)
 #' data("larynx")
 #' cox.fit=coxph(Surv(time, delta)~age+diagyr+factor(stage),data=larynx,method="efron")
